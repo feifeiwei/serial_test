409e0e9124b2cf3f4f4a4281d9a7bba7dfa7b3e8
feifeiwei <40533960+feifeiwei@users.noreply.github.com>
2023-02-15 19:43:46 +0800

    Add files via upload

--- cpp_code/main.cpp
+++ cpp_code/main.cpp
@@ -51,43 +51,90 @@ private:
 };
 
 
+void t28_33_test_init(table_28& t28, table_33& t33)
+{
+    t28.header = 0xeb90;
+    t28.data_len = 0x0705;
+    t28.msg_code = 0x8a;
+    t28.control_type = 0x0;
+    t28.device_ssr = 0x1;
+    t28.device_running_mode = 0x2;
+//    message.num7 = "192.168.1.200:18000";
+    strcpy((char*)t28.addr_rgb, "00000192.168.1.200:18000");
+    strcpy((char*)t28.addr_ir, "00000192.168.1.200:18000");
+    strcpy((char*)t28.addr_lidar, "00000192.168.1.200:18000");
+    strcpy((char*)t28.keep, "keep"); //4bit
+    
+    t28.checkSum= 0xe35;//0x4f;
+    t28.tail= 0xaa;
+    
+    t33 = t28;
+    t33.error_msg = 0x00;
+    t33.checkSum = t33.get_checkSum();
+}
+
+void t29_34_test_init(table_29& t29, table_34& t34)
+{
+    t29.header = 0xeb90;
+    t29.data_len = 0x0705;
+    t29.msg_code = 0x8a;
+    t29.control_type = 0x0;
+    
+    t29.det_isBegin = 0x0;
+    t29.det_pattern = 0x0;
+    t29.det_dataSource = 0x0;
+    t29.preprocess_method = 0x0;
+    t29.obj_conf = 0x0;
+    t29.obj_rec_conf = 0x0;
+    t29.minimum_len = 0x0;
+    t29.minimum_width = 0x0;
+    
+    t29.unknown_det_begin = 0x0;
+    strcpy((char*)t29.keep, "keep"); //4bit
+    
+    t29.checkSum = 0x9551;
+    t29.tail = 0xAA;
+    
+    t34 = t29; //
+    t34.error_msg = 0x00;
+    t34.checkSum = t34.get_checkSum();
+}
+
 int main(void)
 {
     //实例化结构体，并赋值
-    tabele_29_34 message;
-    
-    message.num1 = 0xeb90;
-    message.num2 = 0x0705;
-    message.num3 = 0x8a;
-    message.num4 = 0x0;
-    message.num5 = 0x1;
-    message.num6 = 0x2;
-//    message.num7 = "192.168.1.200:18000";
-    strcpy((char*)message.num7, "00000192.168.1.200:18000");
-    strcpy((char*)message.num8, "00000192.168.1.200:18000");
-    strcpy((char*)message.num9, "00000192.168.1.200:18000");
-    strcpy((char*)message.num10, "00000000000000000000000000000000000000000000000000000000000keep");
-    
-    message.num11= 0x4f;
-    message.num12= 0xaa;
+    table_28 t28;
+    table_33 t33;
+    t28_33_test_init(t28, t33);
     
     Socket_tools sock("127.0.0.1", "127.0.0.1");
-    
-    while (1)
-    {
-        sock.send_msg<tabele_29_34>(message);
-        sleep(1);
-        break;
-    }
-    
-    std::cout <<"Done."<<  message.get_checkSum() << std::endl;
-//    close(sock);
-    std::cout <<"Done."<< std::endl;
-    
-    if (message.is_equal())
+    sock.send_msg<table_28>(t28);
+    sock.send_msg<table_33>(t33);
+        
+    if (t28.is_equal())
     {
         std::cout << "check out is ok!" << std::endl;
+    }else
+    {
+        std::cout << "check out fails!" << std::endl;
     }
+    std::cout <<"t28 checksum: " << t28.checkSum << std::endl;
+    std::cout <<"t33 checksum: " << t33.checkSum << std::endl;
+    
+    std::cout << "------------------------------------------------" << std::endl;
+    table_29 t29;
+    table_34 t34;
+    t29_34_test_init(t29, t34);
+    
+    if (t29.is_equal())
+    {
+        std::cout << "29 check out is ok!" << std::endl;
+    }else
+    {
+        std::cout << "check out fails!" << std::endl;
+    }
+    std::cout <<"t29 checksum: " << t29.get_checkSum() << std::endl;
+    std::cout <<"t34 checksum: " << t34.checkSum << std::endl;
     
     return 0;
 }
--- cpp_code/table_msgs.hpp
+++ cpp_code/table_msgs.hpp
@@ -1,10 +1,9 @@
 //
 //  table_msgs.hpp
-//  demo-learn
+//  serial_test
 //
-//  Created by feifeiwei on 2023/2/13.
+//  Created by brave_mac on 2023/2/14.
 //
-
 #ifndef table_msgs_hpp
 #define table_msgs_hpp
 #include <iostream>
@@ -13,36 +12,345 @@
 
 template<typename T>
 int split_int(T num);
-
 int split_char(unsigned char*);
 
+uint16_t do_crc_checkSum(uint8_t* buff, uint16_t len);
 
-// tabel 29_
+// ------------------------ table 28 33 ------------------------------------------
+// tabel 28 basic control msg
 #pragma pack(push,1)
-struct tabele_29_34    //要发送的数据结构
-{
-    uint16_t num1;
-    uint16_t num2;
-
-    uint8_t num3;
-    uint8_t num4;
-    uint8_t num5;
-    uint8_t num6;
-
-    unsigned char num7[24];  // how to get 24bit type?? string==24bit
-    unsigned char num8[24];
-    unsigned char num9[24];
-    unsigned char num10[63]; // 预留
+struct table_28    //要发送的数据结构
+{   //len = 83(exclude 帧头,帧wei,校验和)
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
     
-    uint8_t num11; //#校验和
-    uint8_t num12;  //#帧尾
+    uint8_t control_type;
+    uint8_t device_ssr;     // devive start stop reset.
+    uint8_t device_running_mode;
+
+    unsigned char addr_rgb[24];  // how to get 24bit type?? string==24bit
+    unsigned char addr_ir[24];
+    unsigned char addr_lidar[24];
+    unsigned char keep[4]; // 预留
+
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
     
-    int get_checkSum();
+    uint16_t get_checkSum(); //获取校验和
     bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+#pragma pack(push,1)
+struct table_33    //要发送的数据结构
+{   //len = 83(exclude 帧头,帧wei,校验和)
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
     
+    
+    uint8_t device_ssr;     // devive start stop reset.
+    uint8_t device_running_mode;
+
+    unsigned char addr_rgb[24];  // how to get 24bit type?? string==24bit
+    unsigned char addr_ir[24];
+    unsigned char addr_lidar[24];
+    unsigned char keep[4]; // 预留
+    
+    uint8_t error_msg;  //#错误码
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    table_33& operator=(const table_28& t28);
+};
+#pragma pack(pop)
+// ------------------------ table 28 33 ------------------------------------------
+
+// ------------------------ table 29 34 ------------------------------------------
+#pragma pack(push,1)
+struct table_29    // control msg
+{   //len = 83(exclude 帧头,帧wei,校验和)
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+    
+    uint8_t det_isBegin;     // object detector start or stop. 00=stop 01=strat
+    uint8_t det_pattern;    // object pattern
+    uint8_t det_dataSource; //
+    uint8_t preprocess_method;
+    
+    uint8_t obj_conf;   //detect confidence
+    uint8_t obj_rec_conf; // recognition confidence
+    
+    uint8_t minimum_len; // for obj detect minimum bbox len.
+    uint8_t minimum_width; // for obj detect minimum bbox w.
+    
+    uint8_t unknown_det_begin; //
+    unsigned char keep[4]; // 预留
+    
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+#pragma pack(push,1)
+struct table_34    // control msg
+{   //len = 83(exclude 帧头,帧wei,校验和)
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+    
+    uint8_t det_isBegin;     // object detector start or stop. 00=stop 01=strat
+    uint8_t det_pattern;    // object pattern
+    uint8_t det_dataSource; //
+    uint8_t preprocess_method;
+    
+    uint8_t obj_conf;   //detect confidence
+    uint8_t obj_rec_conf; // recognition confidence
+    
+    uint8_t minimum_len; // for obj detect minimum bbox len.
+    uint8_t minimum_width; // for obj detect minimum bbox w.
+    
+    uint8_t unknown_det_begin; //
+
+    unsigned char keep[4]; // 预留
+    uint8_t error_msg;  //#错误码
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    table_34& operator=(const table_29& t29);
+};
+#pragma pack(pop)
+// ------------------------ table 29 34 ------------------------------------------
+// ------------------------ table 30 35 ------------------------------------------
+#pragma pack(push,1)
+struct table_30    // object tracking
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+    
+    
+    uint8_t track_isBegin;     // object detector start or stop. 00=stop 01=strat
+    uint8_t track_pattern;    // object pattern
+    uint8_t track_principle; //
+    uint16_t track_id_manua;
+    uint8_t track_conf;   //
+    uint16_t track_maximum; //
+    uint16_t track_free; //
+    
+    unsigned char keep[4]; // 预留
+    
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+#pragma pack(push,1)
+struct table_35    // object tracking
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+    
+    
+    uint8_t track_isBegin;     // object detector start or stop. 00=stop 01=strat
+    uint8_t track_pattern;    // object pattern
+    uint8_t track_principle; //
+    uint16_t track_id_manua;
+    uint8_t track_conf;   //
+    uint16_t track_maximum; //
+    uint16_t track_free; //
+    
+    unsigned char keep[4]; // 预留
+    uint8_t error_msg; //错误码
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    table_35& operator=(const table_30& t30);
+};
+#pragma pack(pop)
+
+// ------------------------ table 30 35 ------------------------------------------
+// ------------------------ table 31 36 ------------------------------------------
+
+#pragma pack(push,1)
+struct table_31    // object alart msg
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+    
+    
+    uint8_t alart_isBegin;     // object detector start or stop. 00=stop 01=strat
+    unsigned char alart_port[24];    // object pattern
+    uint8_t alart_trigger_mode; //
+    uint8_t alart_classes;
+    uint8_t alart_threat_level;   //
+    uint8_t unknownObj_alart_conf; //
+    uint16_t alart_free; //
+    
+    unsigned char keep[4]; // 预留
+    
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+
+#pragma pack(push,1)
+struct table_36    // object alart msg
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+    
+    
+    uint8_t alart_isBegin;     // object detector start or stop. 00=stop 01=strat
+    unsigned char alart_port[24];    // object pattern
+    uint8_t alart_trigger_mode; //
+    uint8_t alart_classes;
+    uint8_t alart_threat_level;   //
+    uint8_t unknownObj_alart_conf; //
+    uint16_t alart_free; //
+    
+    unsigned char keep[4]; // 预留
+    uint8_t error_msg; //错误码
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    table_36& operator=(const table_31& t31);
+};
+#pragma pack(pop)
+
+// ------------------------ table 31 36 ------------------------------------------
+// ------------------------ table 32 37 ------------------------------------------
+#pragma pack(push,1)
+struct table_32    // object detection classes control msg
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+        
+    uint8_t obj_classes;     // see appendix 2
+
+    unsigned char keep[2]; // 预留
+    
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+#pragma pack(push,1)
+struct table_37    // object detection classes control msg
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+    uint8_t control_type;
+        
+    uint8_t obj_classes;     // see appendix 2
+
+    unsigned char keep[2]; // 预留
+    uint8_t error_msg;
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+
+// ------------------------ table 32 37 ------------------------------------------
+// ------------------------ table 38 39 ------------------------------------------
+// device condition query.
+
+#pragma pack(push,1)
+struct table_38    // object detection device‘s condition query.
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+        
+    uint8_t query_type;  // condition or ability query 01H 02H
+
+    unsigned char keep[2]; // 预留
+    
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+
+#pragma pack(push,1)
+struct table_39x    // object detection device‘s condition query.  return msg.
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+        
+    uint8_t feedback_type;  // condition or ability query 01H 02H
+
+    unsigned char keep[2]; // 预留
+    
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
+};
+#pragma pack(pop)
+
+#pragma pack(push,1)
+struct table_40x    // object detection device‘s ability query.  return msg.
+{
+    uint16_t header; //帧头
+    uint16_t data_len; //数据长度
+    uint16_t msg_code;  //消息代码
+        
+    uint8_t feedback_type;  // condition or ability query 01H 02H
+
+    unsigned char keep[2]; // 预留
+    
+    uint16_t checkSum; //#校验和
+    uint8_t tail;  //#帧尾
+    
+    uint16_t get_checkSum(); //成员函数不占空间
+    bool is_equal(); //校验和是否一致。
 };
 #pragma pack(pop)
 
 
 
 #endif /* table_msgs_hpp */
+
+
